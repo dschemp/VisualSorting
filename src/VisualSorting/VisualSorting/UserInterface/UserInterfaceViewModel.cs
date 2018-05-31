2e4f5d58792a76925509456231de164fac405346
Daniel Schemp <annomc1.yt@gmail.com>
2018-05-31 11:52:18 +0200

    CreateCommand, ViewModelBase

--- src/VisualSorting/VisualSorting/UserInterface/UserInterfaceViewModel.cs
+++ src/VisualSorting/VisualSorting/UserInterface/UserInterfaceViewModel.cs
@@ -5,11 +5,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace VisualSorting.UserInterface
 {
-    public class UserInterfaceViewModel
+    public class UserInterfaceViewModel : ViewModelBase
     {
         public SeriesCollection SeriesCollection { get; set; }
 
@@ -27,6 +28,9 @@ namespace VisualSorting.UserInterface
             };
 
             SeriesCollection[0].Values = new ChartValues<int> { 1, 2 };
+
+            Sort = CreateCommand(_ => MessageBox.Show("Sort-Test"));
+            Randomize = CreateCommand(_ => MessageBox.Show("Randomize-Test"));
         }
     }
 }
--- src/VisualSorting/VisualSorting/UserInterface/ViewModelBase.cs
+++ src/VisualSorting/VisualSorting/UserInterface/ViewModelBase.cs
@@ -1,53 +1,200 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace VisualSorting.UserInterface
 {
-    class ViewModelBase
+    public class ViewModelBase : INotifyPropertyChanged
     {
-        public ViewModelBase()
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public static ICommand CreateCommand(Action<object> execute, bool async = false)
         {
-            _canExecute = true;
+            return new ApplicationCommand(execute, null, async);
         }
-        private ICommand _clickCommand;
-        public ICommand ClickCommand
-        {
-            get
-            {
-                return _clickCommand ?? (_clickCommand = new CommandHandler(() => MyAction(), _canExecute));
-            }
-        }
-        private bool _canExecute;
-        public void MyAction()
-        {
 
+
+        public static ICommand CreateCommand(Action<object> execute, Predicate<object> canExecute, bool async = false)
+        {
+            return new ApplicationCommand(execute, canExecute, async);
+        }
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 
-    public class CommandHandler : ICommand
+    public class ApplicationCommand : ICommand
     {
-        private Action _action;
-        private bool _canExecute;
-        public CommandHandler(Action action, bool canExecute)
+        private readonly Lazy<RelayCommand> _innerCommand;
+
+
+        public ApplicationCommand(Action<object> executeAction, Predicate<object> canExecuteAction = null, bool async = false)
         {
-            _action = action;
-            _canExecute = canExecute;
+            _innerCommand = canExecuteAction == null ?
+                new Lazy<RelayCommand>(() => async ? new RelayAsyncCommand(executeAction) : new RelayCommand(executeAction)) :
+                new Lazy<RelayCommand>(() => async ? new RelayAsyncCommand(executeAction, canExecuteAction) : new RelayCommand(executeAction, canExecuteAction));
         }
+
 
         public bool CanExecute(object parameter)
         {
-            return _canExecute;
+            return _innerCommand.Value.CanExecute(parameter);
         }
 
-        public event EventHandler CanExecuteChanged;
 
         public void Execute(object parameter)
         {
-            _action();
+            _innerCommand.Value.Execute(parameter);
+        }
+
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { _innerCommand.Value.CanExecuteChanged += value; }
+            remove { _innerCommand.Value.CanExecuteChanged -= value; }
+        }
+
+
+        public void OnCanExecuteChanged()
+        {
+            _innerCommand.Value.OnCanExecuteChanged();
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        protected Action<object> execute;
+
+        private Predicate<object> canExecute;
+
+        private event EventHandler CanExecuteChangedInternal;
+
+        public RelayCommand(Action<object> execute)
+            : this(execute, DefaultCanExecute)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute ?? throw new ArgumentNullException("execute");
+            this.canExecute = canExecute ?? throw new ArgumentNullException("canExecute");
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+                this.CanExecuteChangedInternal += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+                this.CanExecuteChangedInternal -= value;
+            }
+        }
+
+        public virtual bool CanExecute(object parameter)
+        {
+            return this.canExecute != null && this.canExecute(parameter);
+        }
+
+        public virtual void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            EventHandler handler = this.CanExecuteChangedInternal;
+            if (handler != null)
+            {
+                //DispatcherHelper.BeginInvokeOnUIThread(() => handler.Invoke(this, EventArgs.Empty));
+                handler.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public void Destroy()
+        {
+            this.canExecute = _ => false;
+            this.execute = _ => { return; };
+        }
+
+        private static bool DefaultCanExecute(object parameter)
+        {
+            return true;
+        }
+    }
+
+    public class RelayAsyncCommand : RelayCommand
+    {
+        private bool isExecuting = false;
+
+        public event EventHandler Started;
+
+        public event EventHandler Ended;
+
+        public bool IsExecuting
+        {
+            get { return this.isExecuting; }
+        }
+
+        public RelayAsyncCommand(Action<object> execute, Predicate<object> canExecute)
+            : base(execute, canExecute)
+        {
+        }
+
+        public RelayAsyncCommand(Action<object> execute)
+            : base(execute)
+        {
+        }
+
+        public override Boolean CanExecute(object parameter)
+        {
+            return ((base.CanExecute(parameter)) && (!this.isExecuting));
+        }
+
+        public override void Execute(object parameter)
+        {
+            try
+            {
+                this.isExecuting = true;
+                this.Started?.Invoke(this, EventArgs.Empty);
+
+                Task task = Task.Factory.StartNew(() =>
+                {
+                    try
+                    {
+                        this.execute(parameter);
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                });
+                task.ContinueWith(t =>
+                {
+                    this.OnRunWorkerCompleted(EventArgs.Empty);
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+            }
+            catch (Exception ex)
+            {
+                this.OnRunWorkerCompleted(new RunWorkerCompletedEventArgs(null, ex, true));
+            }
+        }
+
+        private void OnRunWorkerCompleted(EventArgs e)
+        {
+            this.isExecuting = false;
+            this.Ended?.Invoke(this, e);
         }
     }
 }
