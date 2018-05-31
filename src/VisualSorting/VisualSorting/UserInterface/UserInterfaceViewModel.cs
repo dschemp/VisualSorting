e9333d5979eb5ffd712544719b3cfa57cd5022cf
Daniel Schemp <annomc1.yt@gmail.com>
2018-05-31 11:12:10 +0200

    ViewModelBase und LiveChart Graph added

--- src/VisualSorting/VisualSorting/UserInterface/UserInterfaceViewModel.cs
+++ src/VisualSorting/VisualSorting/UserInterface/UserInterfaceViewModel.cs
@@ -1,15 +1,32 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace VisualSorting.UserInterface
 {
     public class UserInterfaceViewModel
     {
+        public SeriesCollection SeriesCollection { get; set; }
 
+        public ICommand Sort { get; set; }
+        public ICommand Randomize { get; set; }
 
+        public UserInterfaceViewModel()
+        {
+            SeriesCollection = new SeriesCollection()
+            {
+                new ColumnSeries()
+                {
+                    Values = new ChartValues<int> { 1 }
+                }
+            };
 
+            SeriesCollection[0].Values = new ChartValues<int> { 1, 2 };
+        }
     }
 }
